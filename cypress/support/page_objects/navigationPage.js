3604f72d7a2d475a3f8ab05c10d2a2844079252b
Daniel <daniel.avilam@opitech.com.co>
2023-08-14 08:51:49 -0500

    Add detailed comments and documentation to NavigationPage class

--- cypress/support/page_objects/navigationPage.js
+++ cypress/support/page_objects/navigationPage.js
@@ -1,5 +1,14 @@
+/**
+ * NavigationPage class provides methods to navigate through different pages 
+ * or categories in a web application. The methods interact with elements 
+ * having the class 'category-cards' and perform clicks on specific headings to 
+ * reach different parts of the application.
+ */
 class NavigationPage {
 
+    /**
+     * Navigates to the "Elements" page by clicking on the first heading that contains the text 'Elements'.
+     */
     elementsPage(){
         cy.get('.category-cards')
         .find('h5')
@@ -8,6 +17,9 @@ class NavigationPage {
         .click()    
     }
 
+    /**
+     * Navigates to the "Forms" page by clicking on the heading with index 1 that contains the text 'Forms'.
+     */
     formsPage(){
         cy.get('.category-cards')
         .find('h5')
@@ -16,6 +28,9 @@ class NavigationPage {
         .click()            
     }
 
+    /**
+     * Navigates to the "Alerts, Frame & Windows" page by clicking on the heading with index 2 that contains the mentioned text.
+     */
     alertsPage(){
         cy.get('.category-cards')
         .find('h5')
@@ -24,6 +39,9 @@ class NavigationPage {
         .click()            
     }
 
+    /**
+     * Navigates to the "Widgets" page by clicking on the heading with index 3 that contains the text 'Widgets'.
+     */
     widgetsPage(){
         cy.get('.category-cards')
         .find('h5')
@@ -32,7 +50,10 @@ class NavigationPage {
         .click()            
     }
 
-    widgetsPage(){
+    /**
+     * Navigates to the "Interactions" page by clicking on the heading with index 4 that contains the text 'Interactions'.
+     */
+    interactionsPage(){
         cy.get('.category-cards')
         .find('h5')
         .eq(4)
@@ -40,6 +61,9 @@ class NavigationPage {
         .click()            
     }    
 
+    /**
+     * Navigates to the "Book Store Application" page by clicking on the last heading that contains the mentioned text.
+     */
     bookStorePage(){
         cy.get('.category-cards')
         .find('h5')
@@ -50,4 +74,5 @@ class NavigationPage {
 
 }
 
+// Export an instance of the NavigationPage class.
 export const navigateTo = new NavigationPage
