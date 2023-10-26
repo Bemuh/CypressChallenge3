9dc7c30014565934dacc31c2ed9037168c40c390
Daniel <daniel.avilam@opitech.com.co>
2023-10-26 08:52:29 -0500

    updated and added files to generate reports

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,17 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   viewportHeight: 1080,
   viewportWidth: 1920,
+
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    configFile: 'reporter-config.json',
+    },
+
+  retries:{
+    runMode: 5,
+    openMode: 0
+  },
+
   e2e: {
     baseUrl: 'https://demoqa.com/',
     setupNodeEvents(on, config) {
